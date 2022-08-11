e4c928aa0f89082a162e585026ba15c8df180678
Yudhatama Gusdi Rahmatullah <87884842+yudhatamarahmatullah123@users.noreply.github.com>
2022-08-11 17:17:13 +0700

    Add files via upload

---
+++ Tugas 5 PBO (Yudhatama)/Tugas_PBO_Praktikum_Pagi/src/main/java/implementasi_2_Contoh_Polymorphism/InterfaceNotifikasi.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package implementasi_2_Contoh_Polymorphism;
+
+/**
+ *
+ * @author anonymous
+ */
+public interface InterfaceNotifikasi {
+    
+    void sendMessage (String receiver, String content);
+    
+}
